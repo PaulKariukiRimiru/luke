34f4f8825c14bd8e22dafea968ccbb24ce718058
Paul Rimiru <paulrimiru@gmail.com>
2018-06-28 21:34:08 +0300

    [feature] restify the bot and add chat connector

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+var restify = require('restify');
 var builder = require('botbuilder');
-var connector = new builder.ConsoleConnector().listen();
+
+// setting up restify
+var server = restify.createServer()
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log('%s is listening am at %s', server.name, server.url)
+});
+
+// Chat connector for communicating with the bot service
+var connector = new builder.ChatConnector({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword
+});
+// listen for messages from user
+server.post('/api/messages', connector.listen())
+
+// receive message and echo them back
 var bot = new builder.UniversalBot(connector, (session) => {
   session.send('you mean %s', session.message.text)
 });
