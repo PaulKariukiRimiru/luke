c3ef3f7239060bac6c626997d20674a9d1d33d2b
Paul Rimiru <paulrimiru@gmail.com>
2018-07-16 14:07:52 +0300

    [Feauture] First conversation

--- index.js
+++ index.js
@@ -17,5 +17,43 @@ server.post('/api/messages', connector.listen())
 
 // receive message and echo them back
 var bot = new builder.UniversalBot(connector, (session) => {
-  session.send('you mean %s', session.message.text)
+  session.beginDialog('know_user');
 });
+
+bot.dialog('know_user', [
+  (session) => {
+    builder.Prompts.text(session, 'Hey there stranger whats your name?')
+  },
+  (session, results) => {
+    if (results.response === 'Krants') {
+      session.beginDialog('dad_talk');
+    } else {
+      builder.Prompts.text(session, `nice to meet you ${results.response}, how can i help?`);
+    }
+  },
+  (session, results) => {
+    builder.Prompts.text(session, `let me check if i can ${results.response} ill get back at you`)
+  }
+])
+
+bot.dialog('dad_talk', [
+  (session) => {
+    builder.Prompts.text(session, 'I hate it when people do this,  whats your real name?');
+  },
+  (session, results) => {
+    if (results.response === 'Krants'){
+      builder.Prompts.text(session, 'You know you really have some nerve am just gonna shut this down!!')
+    } else {
+      session.endDialogWithResult({
+        response: result,
+      })
+    }
+  },
+  (session, result) => {
+    if (result.response === 'ciru') {
+      builder.Prompts.text(session, 'Hey dad what you up to?');
+    } else {
+      session.endDialog(session, 'Bye who ever you are');
+    }
+  }
+])
